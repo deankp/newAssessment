69916355b1488cd5ecf5ff59797cc16d0313f658
deankp <dperez0689@gmail.com>
2022-02-23 21:09:27 -0600

    more css

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ let changeSize = document.getElementById("shrink-grow")
 
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
-  if (image.style.height == "100px") {
+  if (image.style.height == "300px") {
     image.style.height = "25px"
   }
   else {
-    image.style.height = "100px"
+    image.style.height = "300px"
   }
 })
